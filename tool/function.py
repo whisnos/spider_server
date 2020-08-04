e0105f2a2e5f0165d139713d5f311b48aa35e88b
whisnos <whisnos@163.com>
2020-08-04 10:37:52 +0800

    dev

--- tool/function.py
+++ tool/function.py
@@ -60,7 +60,7 @@ def return_sqlserver_connect():
     try:
         conn = pymssql.connect(host='172.17.146.39', port='1433', user='sa', password='shanpengfei@no1',
                                database='TaoKe')
-        cur = conn.cursor()
+        cur = conn.cursor(as_dict=True)
     except Exception as e:
         return False, False
     return cur, conn
